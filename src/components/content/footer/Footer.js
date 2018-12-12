74acf56f39aa14b9a469c913f0bd95553736ac02
Ashu <ashupal0005@gmail.com>
2018-12-13 04:37:39 +0530

    slider changes

--- src/components/content/footer/Footer.js
+++ src/components/content/footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Footer.css';
-import MOMGUIDE_LOGO_WHITE from '../../../assets/images/MOMGUIDE_LOGO_WHITE.png';
+// import MOMGUIDE_LOGO_WHITE from '../../../assets/images/MOMGUIDE_LOGO_WHITE.png';
 
 
 
--- src/components/content/slider/Slider.js
+++ src/components/content/slider/Slider.js
@@ -3,7 +3,9 @@ import React from 'react';
 // import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
 import '../../../../node_modules/react-bootstrap-carousel/dist/react-bootstrap-carousel.css';
 import './Slider.css';
-import la from '../../../assets/images/la.jpg';
+// import $ from 'jquery';
+// $('.carousel').carousel();
+// import la from '../../../assets/images/la.jpg';
 // import MOMGUIDE_LOGO_WHITE from '../../../assets/images/MOMGUIDE_LOGO_WHITE.png';
 
 
@@ -14,28 +16,31 @@ export const Slider = ()=>{
                 <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel" data-interval="3000">
                     <ol className="carousel-indicators">
                         <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1" className="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2" className="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                     </ol>
-                    <div className="carousel-item">
-                        <img src={require('../../../assets/images/la.jpg')} className="" alt="First Slide" />
-                    </div> 
-                    
-                    <div className="carousel-item">
-                        <img src={require('../../../assets/images/la.jpg')} className="" alt="Second Slide"/>
-                    </div> 
-                    <div className="carousel-item">
-                        <img src={require('../../../assets/images/la.jpg')} className="" alt="Third Slide"/>
-                    </div> 
-                </div>
-                <a href="#carouselExampleIndicators" className="carousel-control-prev slider_prev" role="button" data-slide="prev">
-                    <span carousel="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Previous</span>
-                </a>
-                <a href="#carouselExampleIndicators" className="carousel-control-prev slider_next" role="button" data-slide="next">
-                    <span carousel="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Next</span>
-                </a>
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img src={require('../../../assets/images/la.jpg')} className="d-block w-100" alt="First Slide" />
+                        </div> 
+                        
+                        <div className="carousel-item">
+                            <img src={require('../../../assets/images/la.jpg')} className="d-block w-100" alt="Second Slide"/>
+                        </div> 
+                        <div className="carousel-item">
+                            <img src={require('../../../assets/images/la.jpg')} className="d-block w-100" alt="Third Slide"/>
+                        </div> 
+                    </div>    
+                
+                    <a href="#carouselExampleIndicators" className="carousel-control-prev" role="button" data-slide="prev">
+                        <span carousel="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Previous</span>
+                    </a>
+                    <a href="#carouselExampleIndicators" className="carousel-control-next" role="button" data-slide="next">
+                        <span carousel="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Next</span>
+                    </a>
+                </div>    
 
             </div>   
         </div>    
