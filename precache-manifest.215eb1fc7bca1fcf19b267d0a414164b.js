cf32d65d3582d2f4c4c06e1c8d387e20f4073d40
iashupal <ashupal0005@gmail.com>
2018-12-13 16:34:24 +0530

    Updates

---
+++ precache-manifest.215eb1fc7bca1fcf19b267d0a414164b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/infogreenlive/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "9c61935136a9fe12f24e",
+    "url": "/infogreenlive/static/css/main.0dec0b40.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/infogreenlive/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "ce5cf353806a6ca20bce",
+    "url": "/infogreenlive/static/js/1.ce5cf353.chunk.js"
+  },
+  {
+    "revision": "75263b035b4cc82bee1f",
+    "url": "/infogreenlive/static/js/runtime~main.75263b03.js"
+  },
+  {
+    "revision": "276db975b83041329ea1acbdf3447c41",
+    "url": "/infogreenlive/static/media/fabric1.276db975.jpg"
+  },
+  {
+    "revision": "f2686b2afdd63e515502de1b879abe0c",
+    "url": "/infogreenlive/static/media/la.f2686b2a.jpg"
+  },
+  {
+    "revision": "249322b60d0237d04ca096ed76b0f4a1",
+    "url": "/infogreenlive/static/media/MOMGUIDE_LOGO_WHITE.249322b6.png"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/infogreenlive/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/infogreenlive/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "9c61935136a9fe12f24e",
+    "url": "/infogreenlive/static/js/main.9c619351.chunk.js"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/infogreenlive/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "04f2e28835d32055e0ff0d7b4ceb78e6",
+    "url": "/infogreenlive/static/media/tips-img-home.04f2e288.jpg"
+  },
+  {
+    "revision": "6209a153d60bb014ee996177c94a1a39",
+    "url": "/infogreenlive/static/media/events-img-home.6209a153.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/infogreenlive/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/infogreenlive/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/infogreenlive/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/infogreenlive/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "ce5cf353806a6ca20bce",
+    "url": "/infogreenlive/static/css/1.33859564.chunk.css"
+  },
+  {
+    "revision": "cd2bdf2d6b763a52ef65a20c89050545",
+    "url": "/infogreenlive/index.html"
+  }
+];
